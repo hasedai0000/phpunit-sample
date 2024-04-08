007733d7ebf67f5f2e989702769078d609b7f7d2
hasedai0000 <hasegawa.daichi.workman@gmail.com>
2024-04-08 17:55:34 +0900

    テストコードの実装

--- src/tests/Unit/CalculatorTest.php
+++ src/tests/Unit/CalculatorTest.php
@@ -2,17 +2,32 @@
 
 namespace Tests\Unit;
 
+use App\Calculator;
 use PHPUnit\Framework\TestCase;
 
 class CalculatorTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    public function testMultiply()
     {
-        $this->assertTrue(true);
+        // Calculatorクラスのインスタンスを生成
+        $target = new Calculator();
+        // 期待値を設定
+        $expected = 12;
+        // 実測値を設定
+        $actual = $target->multiply(3, 4);
+        // 実測値と期待値が等しいか検証
+        $this->assertSame($expected, $actual);
+    }
+
+    public function testSum()
+    {
+        // Calculatorクラスのインスタンスを生成する
+        $target = new Calculator();
+        // 想定する計算結果を用意
+        $expected = 12;
+        // sumメソッドを呼び出し、実測値を用意
+        $actual = $target->sum(2, 8);
+        // 期待値と実測値の値を比較して想定する結果と等しいか検証
+        $this->assertSame($expected, $$actual);
     }
 }
